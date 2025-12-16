561da9879a070152f1c2eb734c1628e3aaec8bb8
platinum1154 <platinum1154@outlook.com>
2025-12-16 10:33:59 +0800

    ✨ feat(AI数据传输): 实现文字表达找不同
    可以传入图片链接，文字描述图片差别

--- src/ImageDiff/__init__.py
+++
@@ -1,5 +0,0 @@
-# import interface.GPT.aligpt as gpt
-
-
-def main() -> None:
-    print("Hello from imagediff!")
--- src/ImageDiff/application/main.py
+++ src/ImageDiff/application/main.py
@@ -1,74 +1,22 @@
-from openai import OpenAI
-from dotenv import load_dotenv
-import os
+import ImageDiff.interface.GPT.ali as ali_gpt
+
+def main() -> None:
+    print("Hello from imagediff!")
+    img1 = "https://pic1.imgdb.cn/item/6940bd5f4a4e4213d00b3d6a.png"
+    img2 = "https://pic1.imgdb.cn/item/6940bd5e4a4e4213d00b3d68.png"
+
+    result = ali_gpt.ask_vl_model(
+        image_a_url=img1,
+        image_b_url=img2,
+        question="这两张图片有什么区别？"
+    )
+    print("\n\n思考过程：")
+    print(result["reasoning"])
+    print("\n\n回答：")
+    print(result["answer"])
 
 
-# 加载环境变量
-load_dotenv()  # 自动加载根目录 .env 文件
 
-# 初始化OpenAI客户端
-client = OpenAI(
-    api_key = os.getenv("DASHSCOPE_API_KEY"),
-    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
-)
-
-reasoning_content = ""  # 定义完整思考过程
-answer_content = ""     # 定义完整回复
-is_answering = False   # 判断是否结束思考过程并开始回复
-enable_thinking = False
-# 创建聊天完成请求
-completion = client.chat.completions.create(
-    model="qwen3-vl-flash",
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": "https://img.alicdn.com/imgextra/i1/O1CN01gDEY8M1W114Hi3XcN_!!6000000002727-0-tps-1024-406.jpg"
-                    },
-                },
-                {"type": "text", "text": "这道题怎么解答？"},
-            ],
-        },
-    ],
-    stream=True,
-    # enable_thinking 参数开启思考过程，thinking_budget 参数设置最大推理过程 Token 数
-    extra_body={
-        'enable_thinking': True,
-        "thinking_budget": 81920},
-
-    # 解除以下注释会在最后一个chunk返回Token使用量
-    # stream_options={
-    #     "include_usage": True
-    # }
-)
-
-if enable_thinking:
-    print("\n" + "=" * 20 + "思考过程" + "=" * 20 + "\n")
-
-for chunk in completion:
-    # 如果chunk.choices为空，则打印usage
-    if not chunk.choices:
-        print("\nUsage:")
-        print(chunk.usage)
-    else:
-        delta = chunk.choices[0].delta
-        # 打印思考过程
-        if hasattr(delta, 'reasoning_content') and delta.reasoning_content != None:
-            print(delta.reasoning_content, end='', flush=True)
-            reasoning_content += delta.reasoning_content
-        else:
-            # 开始回复
-            if delta.content != "" and is_answering is False:
-                print("\n" + "=" * 20 + "完整回复" + "=" * 20 + "\n")
-                is_answering = True
-            # 打印回复过程
-            print(delta.content, end='', flush=True)
-            answer_content += delta.content
-
-# print("=" * 20 + "完整思考过程" + "=" * 20 + "\n")
-# print(reasoning_content)
-# print("=" * 20 + "完整回复" + "=" * 20 + "\n")
-# print(answer_content)
+    
+if __name__ == "__main__":
+    main()
